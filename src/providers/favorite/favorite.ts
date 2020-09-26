6382daa3a782539df6dd6ba502c3c173236eb685
Nehal Jaisalmeria <nehal.jaisalmeria14@gmail.com>
2020-09-26 14:30:17 +0530

    Assignment 3: Grid and Storage

--- src/providers/favorite/favorite.ts
+++ src/providers/favorite/favorite.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Dish } from '../../shared/dish';
 import { Observable } from 'rxjs/Observable';
 import { DishProvider } from '../dish/dish';
+import { Storage } from '@ionic/storage';
 
 /*
   Generated class for the FavoriteProvider provider.
@@ -16,14 +17,22 @@ export class FavoriteProvider {
   favorites: Array<any>;
 
   constructor(public http: HttpClient,
-    private dishservice: DishProvider) {
+    private dishservice: DishProvider,
+    private storage: Storage) {
     console.log('Hello FavoriteProvider Provider');
-    this.favorites = [];
+    storage.get('favorites').then(favorites => {
+      if (favorites)
+        this.favorites = favorites;
+      else
+        this.favorites = [];
+    });
   }
 
   addFavorite(id: number): boolean {
-    if (!this.isFavorite(id))
+    if (!this.isFavorite(id)) {
       this.favorites.push(id);
+      this.storage.set('favorites', this.favorites);
+    }
     console.log('favorites', this.favorites);
     return true;
   }
@@ -41,6 +50,7 @@ export class FavoriteProvider {
     let index = this.favorites.indexOf(id);
     if (index >= 0) {
       this.favorites.splice(index,1);
+      this.storage.set('favorites', this.favorites);
       return this.getFavorites();
     }
     else {
